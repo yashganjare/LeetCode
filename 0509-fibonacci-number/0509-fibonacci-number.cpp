e1d4adb08fd192a2139e6ade13ce1163f8daad96
Yash Ganjare <166136921+yashganjare@users.noreply.github.com>
2026-07-17 21:35:18 +0530

    Time: 14 ms (22.87%), Space: 7.8 MB (65.21%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.cpp
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -3,13 +3,6 @@ public:
     int fib(int n) {
         if(n==0) return 0;
         if(n==1) return 1;
-        vector<int>ans(n+1);
-        ans[0]=0;
-        ans[1]=1;
-        for(int i=2;i<n+1;i++)
-        {
-            ans[i]=ans[i-1]+ans[i-2];
-        }
-        return ans[n];
+        return fib(n-1)+fib(n-2);
     }
 };
