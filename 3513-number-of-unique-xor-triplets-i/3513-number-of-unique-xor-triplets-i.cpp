bc751d647e0854f1216835990e76d738990da547
Yash Ganjare <166136921+yashganjare@users.noreply.github.com>
2026-07-23 13:37:37 +0530

    Time: 4 ms (3.13%), Space: 198.8 MB (85%) - LeetHub

---
+++ 3513-number-of-unique-xor-triplets-i/3513-number-of-unique-xor-triplets-i.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int uniqueXorTriplets(vector<int>& nums) {
+        int n=nums.size();
+        if(n<=2) return n;
+        int mask=0;
+        for(int num:nums)
+        {
+            mask|=num;
+        }
+        return mask+1;
+    }
+};
